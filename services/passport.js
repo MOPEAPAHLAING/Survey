d43894963f9f3de86b2e97d0c282b81957d8fe24
Moe Pa Pa Hlaing <moepapahlaing@ucsm.edu.mm>
2020-09-19 12:07:25 +0630

    Initial

--- services/passport.js
+++ services/passport.js
@@ -21,19 +21,20 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-        if(existingUser){
-            //we already have a record with the given profile ID
-            done(null, existingUser);
+}, 
+    async (accessToken, refreshToken, profile, done) => {
+        try{
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if(existingUser){
+                return done(null, existingUser);
+            }
+            
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
+        }catch(err){
+            console.log(err);
         }
-        else{
-            //we don't have a user record with this ID, make a new record
-            new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-    });
+    
+    
 })
 );
